4b5d32c93dc3fb32cbfeac9ccb8bbd88e4e4825a
Isaac <email@isaacreyna.com>
2016-11-03 16:02:31 -0500

    Added Picasso library. Displays Profile Picture. Add Picasso @Override example

--- app/src/main/java/com/isaacreyna/employeetimeclock/MainActivity.java
+++ app/src/main/java/com/isaacreyna/employeetimeclock/MainActivity.java
@@ -2,6 +2,9 @@ package com.isaacreyna.employeetimeclock;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -17,11 +20,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.gson.Gson;
 import com.isaacreyna.employeetimeclock.models.User;
+import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Target;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener, ExampleFragment.OnFragmentInteractionListener {
@@ -63,13 +70,38 @@ public class MainActivity extends AppCompatActivity
     }
 
     void setNavHeader(User user){
-        View navView = LayoutInflater.from(this).inflate(R.layout.nav_header_main, null); //navigation header menu layout
+        final View navView = LayoutInflater.from(this).inflate(R.layout.nav_header_main, null); //navigation header menu layout
 
         TextView name = (TextView) navView.findViewById(R.id.fullname);
-        name.setText(user.first_name);
+        name.setText(user.DisplayName());
 
         TextView email = (TextView) navView.findViewById(R.id.email);
         email.setText(user.email);
+
+        //Loading an image
+        String url = "http://www.isaacreyna.com/system/profile/uploads/"+user.photo;
+        ImageView iv = (ImageView) navView.findViewById(R.id.imageView);
+        Picasso.with(getApplicationContext()).load(url).fit().into(iv);
+        /**/
+        Picasso.with(this).load(url).into(new Target() {
+
+            @Override
+            public void onBitmapLoaded (final Bitmap bitmap, Picasso.LoadedFrom from){
+                Drawable drawable = new BitmapDrawable(getApplicationContext().getResources(), bitmap);
+                navView.findViewById(R.id.BackgroundImageView).setBackground(drawable);
+            }
+
+            @Override
+            public void onBitmapFailed(final Drawable errorDrawable) {
+                Log.d("TAG", "FAILED");
+            }
+
+            @Override
+            public void onPrepareLoad(final Drawable placeHolderDrawable) {
+                Log.d("TAG", "Prepare Load");
+            }
+        });
+        /**/
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
--- app/src/main/java/com/isaacreyna/employeetimeclock/models/User.java
+++ app/src/main/java/com/isaacreyna/employeetimeclock/models/User.java
@@ -18,7 +18,18 @@ public class User {
     public String city;
     public String state;
     public String zip_code;
-    public String default_company;
+    public int default_company;
+
+    public String DisplayName(){
+        String name;
+        if ((this.first_name == null) || (this.middle_name == null) || (this.last_name == null))
+            name = this.username;
+        else if (this.first_name.isEmpty() || this.middle_name.isEmpty() || this.last_name.isEmpty())
+            name = this.username;
+        else
+            name = this.first_name + " " + this.middle_name + " " + this.last_name;
+     return name;
+    }
 }
 
 
